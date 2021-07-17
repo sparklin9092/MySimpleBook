4ba4776cee463b4b319daaeda9ff4ef5e4f83dcd
Spark Lin <sparklin9092@gmail.com>
2021-07-17 13:07:15 +0800

    刪除無用的import

--- src/main/java/net/spark9092/MySimpleBook/controller/UserCheckController.java
+++ src/main/java/net/spark9092/MySimpleBook/controller/UserCheckController.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.spark9092.MySimpleBook.dto.UserCheckMsgDto;
@@ -14,31 +13,31 @@ import net.spark9092.MySimpleBook.enums.SessinNameEnum;
 
 @RestController
 public class UserCheckController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(UserInfoController.class);
-	
+
 	@GetMapping("/userCheck")
 	public UserCheckMsgDto userCheck(HttpSession session) {
-		
+
 		UserCheckMsgDto userCheckMsgDto = new UserCheckMsgDto();
-		
+
 		UserInfoEntity userInfoEntity = (UserInfoEntity) session.getAttribute(SessinNameEnum.USER_INFO.getName());
-		
+
 		if(null == userInfoEntity) {
-			
+
 			userCheckMsgDto.setStatus(false);
 			userCheckMsgDto.setMsg("使用者未登入");
 			userCheckMsgDto.setUserName("");
-			
+
 		} else {
 
 			logger.debug(userInfoEntity.toString());
-			
+
 			userCheckMsgDto.setStatus(true);
 			userCheckMsgDto.setMsg("");
 			userCheckMsgDto.setUserName(userInfoEntity.getUserName());
 		}
-		
+
 		return userCheckMsgDto;
 	}
 
