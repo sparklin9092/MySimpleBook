33e5695200c2ea8ada0e425fb1bb0f4e14ca556b
Spark Lin <sparklin9092@gmail.com>
2021-07-19 08:00:52 +0800

    使用第一個測試類，印出debug的log

--- src/test/java/net/spark9092/MySimpleBook/MySimpleBookApplicationTests.java
+++ src/test/java/net/spark9092/MySimpleBook/MySimpleBookApplicationTests.java
@@ -1,13 +1,20 @@
 package net.spark9092.MySimpleBook;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import net.spark9092.MySimpleBook.service.UserLoginService;
 
 @SpringBootTest
 class MySimpleBookApplicationTests {
 
+	private static final Logger logger = LoggerFactory.getLogger(UserLoginService.class);
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		logger.debug("第一個測試");
 	}
 
 }
