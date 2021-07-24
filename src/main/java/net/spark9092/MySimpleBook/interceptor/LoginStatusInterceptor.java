5078ddd814e5d494310636eac5201c27e3d36218
Spark Lin <sparklin9092@gmail.com>
2021-07-24 15:55:42 +0800

    修改登入狀態攔截的LOG列印內容

--- src/main/java/net/spark9092/MySimpleBook/interceptor/LoginStatusInterceptor.java
+++ src/main/java/net/spark9092/MySimpleBook/interceptor/LoginStatusInterceptor.java
@@ -3,6 +3,8 @@ package net.spark9092.MySimpleBook.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -12,17 +14,21 @@ import net.spark9092.MySimpleBook.enums.SessinNameEnum;
 @Component
 public class LoginStatusInterceptor implements HandlerInterceptor {
 
+	private static final Logger logger = LoggerFactory.getLogger(LoginStatusInterceptor.class);
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
-		System.out.println("登入攔截成功！");
+		logger.info("登入攔截，Request URL：{}", request.getServletPath());
 
 		UserInfoEntity userInfoEntity = (UserInfoEntity) request.getSession().getAttribute(SessinNameEnum.USER_INFO.getName());
 
 		if (null == userInfoEntity) {
+			
+			logger.info("使用者未登入，返回登入頁");
 
 			response.sendRedirect("/");
-			
+
 			return false;
 
 		} else {
