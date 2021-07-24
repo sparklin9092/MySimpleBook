3cd80bd8d194bbbc78df60658fee9a73d5f65b8f
Spark Lin <sparklin9092@gmail.com>
2021-07-24 09:28:19 +0800

    修正讀取白名單時，會發生null的問題

--- src/main/java/net/spark9092/MySimpleBook/mapper/ISystemWhiteListMapper.java
+++ src/main/java/net/spark9092/MySimpleBook/mapper/ISystemWhiteListMapper.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 import net.spark9092.MySimpleBook.dto.whiteList.WhiteListValueDto;
@@ -14,5 +16,9 @@ public interface ISystemWhiteListMapper {
 	@Select("select id, white_value from system_whitelist "
 			+ " where white_type=#{whiteType} "
 			+ " and is_active=1 and is_delete=0")
+	@Results({
+		@Result(column="id", property="id"),
+		@Result(column="white_value", property="value")
+	})
 	List<WhiteListValueDto> selectListByType(@Param("whiteType") String whiteType);
 }
