9692ca2f4eb977d9ba1dff3b0ca77c1584caba2d
Spark Lin <sparklin9092@gmail.com>
2021-07-24 21:54:22 +0800

    新增加解密公用類，為使用者密碼加密，加密技術還未定

---
+++ src/main/java/net/spark9092/MySimpleBook/common/CryptionCommon.java
@@ -0,0 +1,21 @@
+package net.spark9092.MySimpleBook.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CryptionCommon {
+
+	public String encryptionPwd(String userPwd) {
+		
+		//TODO 為使用者密碼進行加密處理
+		
+		return userPwd;
+	}
+
+	public String decryptionPwd(String userPwd) {
+		
+		//TODO 為使用者密碼進行解密處理
+		
+		return userPwd;
+	}
+}
