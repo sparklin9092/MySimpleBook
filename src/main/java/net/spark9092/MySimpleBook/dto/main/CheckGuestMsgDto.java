5da69388e7e5254c0d30c89a506bec1cf8d0be0f
Spark Lin <sparklin9092@gmail.com>
2021-07-26 12:04:49 +0800

    修補首頁檢查訪客第一次登入時，找不到Dto的問題

---
+++ src/main/java/net/spark9092/MySimpleBook/dto/main/CheckGuestMsgDto.java
@@ -0,0 +1,15 @@
+package net.spark9092.MySimpleBook.dto.main;
+
+public class CheckGuestMsgDto {
+	
+	private boolean status;
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+}
