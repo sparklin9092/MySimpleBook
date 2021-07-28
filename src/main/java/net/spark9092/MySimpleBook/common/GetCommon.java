b2f69905a34ea0b36055f1748ded4c0353158fe9
Spark Lin <sparklin9092@gmail.com>
2021-07-28 20:03:24 +0800

    GetCommon(取得公用類)增加對日期的格式化方法，去處理建立日期顯示錯誤的問題

--- src/main/java/net/spark9092/MySimpleBook/common/GetCommon.java
+++ src/main/java/net/spark9092/MySimpleBook/common/GetCommon.java
@@ -1,13 +1,19 @@
 package net.spark9092.MySimpleBook.common;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GetCommon {
+
+	private static final Logger logger = LoggerFactory.getLogger(GetCommon.class);
 
 	public String getIpAddress(HttpServletRequest request) throws IOException {
 		// 获取请求主机IP地址,如果通过代理进来，则透过防火墙获取真实IP地址
@@ -40,5 +46,26 @@ public class GetCommon {
 			}
 		}
 		return ip;
+	}
+	
+	/**
+	 * 格式化日期，輸出為「yyyy年MM月dd日」
+	 * @param dataTime (java.time.LocalDateTime)
+	 * @return
+	 */
+	public String getFormatDate(LocalDateTime dataTime) {
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy年MM月dd日");
+		
+		try {
+			
+			return dataTime.format(formatter);
+			
+		} catch (Exception e) {
+			
+			logger.error("格式化日期發生錯誤！！！");
+			
+			return "";
+		}
 	}
 }
