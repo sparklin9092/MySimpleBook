4a7e2734ba47604fa4afa55b9fe1eff81fb791cb
Spark Lin <sparklin9092@gmail.com>
2021-07-31 07:39:14 +0800

    查詢支出紀錄時，增加查詢延遲，日期範圍越大，延遲等待越久，最久不超過1秒

--- src/main/webapp/js/spend/records.js
+++ src/main/webapp/js/spend/records.js
@@ -21,69 +21,73 @@ function initSpendDataTable(startDate, endDate) {
 	data.startDate = startDate;
 	data.endDate = endDate;
 	
-	if($.fn.DataTable.isDataTable('#spendRecTable')) {
-		$('#spendRecTable').dataTable().fnDestroy();
-		$('#spendRecTable').empty();
-	}
+	setTimeout(function() {
+		if($.fn.DataTable.isDataTable('#spendRecTable')) {
+			$('#spendRecTable').dataTable().fnDestroy();
+			$('#spendRecTable').empty();
+		}
+	}, 100);
 	
-	$.ajax({
-		url: '/spend/records',
-		method: 'POST',
-		dataType: 'json',
-		contentType: 'application/json',
-		data: JSON.stringify(data),
-		success: function(res) {
-			
-			var data = [];
-			
-			if(res.status) {
+	setTimeout(function() {
+		$.ajax({
+			url: '/spend/records',
+			method: 'POST',
+			dataType: 'json',
+			contentType: 'application/json',
+			data: JSON.stringify(data),
+			success: function(res) {
 				
-				data = res.list
-			}
-			
-			$('#spendRecTable').DataTable({
-				dom: '<"float-start"f>tp',
-				data: data,
-				columns: [
-					{title: 'id'},
-					{title: '時間'},
-					{title: '項目'},
-					{title: '金額'},
-					{title: '功能'}
-				],
-				order: [[1, 'desc']],
-				columnDefs: [
-					{
-						targets: 0,
-						visible: false,
-						searchable: false
-					},
-					{
-						targets: 4,
-						orderable: false,
-						render: function(data, type, row) {
-							return '<button type="button" class="btn btn-outline-warning" onclick="modifyView('+row[0]+')">修改</button>';
+				var data = [];
+				
+				if(res.status) {
+					
+					data = res.list
+				}
+				
+				$('#spendRecTable').DataTable({
+					dom: '<"float-start"f>tp',
+					data: data,
+					columns: [
+						{title: 'id'},
+						{title: '時間'},
+						{title: '項目'},
+						{title: '金額'},
+						{title: '功能'}
+					],
+					order: [[1, 'desc']],
+					columnDefs: [
+						{
+							targets: 0,
+							visible: false,
+							searchable: false
+						},
+						{
+							targets: 4,
+							orderable: false,
+							render: function(data, type, row) {
+								return '<button type="button" class="btn btn-outline-warning" onclick="modifyView('+row[0]+')">修改</button>';
+							}
+						}
+					],
+					pageLength: 5,
+					language: {
+						search: '搜尋：',
+						zeroRecords: '找不到資料',
+						paginate: {
+							first: '第一頁',
+							last: '最後一頁',
+							next: '下一頁',
+							previous: '上一頁'
 						}
 					}
-				],
-				pageLength: 5,
-				language: {
-					search: '搜尋：',
-					zeroRecords: '找不到資料',
-					paginate: {
-						first: '第一頁',
-						last: '最後一頁',
-						next: '下一頁',
-						previous: '上一頁'
-					}
-				}
-			});
-		},
-		error: function(err) {
-			console.log(err);
-			alert('無法連接伺服器');
-		}
-	});
+				});
+			},
+			error: function(err) {
+				console.log(err);
+				alert('無法連接伺服器');
+			}
+		});
+	}, 100);
 }
 
 function modifyView(transId) {
@@ -114,21 +118,27 @@ function changeDataRange() {
 					var startDate = moment().day(1).format('YYYY-MM-DD');
 					var endDate = moment().day(7).format('YYYY-MM-DD');
 					
-					initSpendDataTable(startDate, endDate);
+					setTimeout(function() {
+						initSpendDataTable(startDate, endDate);
+					}, 100);
 					
 				} else if(dateRange == 30) {
 					
 					var startDate = moment().startOf('month').format('YYYY-MM-DD');
 					var endDate = moment().endOf('month').format('YYYY-MM-DD');
 					
-					initSpendDataTable(startDate, endDate);
+					setTimeout(function() {
+						initSpendDataTable(startDate, endDate);
+					}, 300);
 					
 				} else if(dateRange == 90) {
 					
 					var startDate = moment().subtract(2, 'months').startOf('month').format('YYYY-MM-DD');
 					var endDate = moment().endOf('month').format('YYYY-MM-DD');
 					
-					initSpendDataTable(startDate, endDate);
+					setTimeout(function() {
+						initSpendDataTable(startDate, endDate);
+					}, 500);
 					
 				} else {
 					
