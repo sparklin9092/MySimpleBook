71227896262fb40849c79628490f9f55e3eea141
Spark Lin <sparklin9092@gmail.com>
2021-08-17 10:27:25 +0800

    統一管理 電子信箱(Email)驗證的 post ajax，降低日後維護成本

--- src/main/webapp/js/verify/mail.js
+++ src/main/webapp/js/verify/mail.js
@@ -20,60 +20,34 @@ function initBuAcc() {
 	var data = {};
 	data.buAcc = $('#buAcc').val();
 	
-	$.ajax({
-		url: '/verify/mail/buacc',
-		method: 'POST',
-		dataType: 'json',
-		contentType: 'application/json',
-		data: JSON.stringify(data),
-		success: function(res) {
-			
-			if(res.status) {
-				
-				if(res.used) {
-					
-					showMsg("您的信箱已經綁定完成！請使用信箱進行登入。");
-					location.href = '/';
-					
-				} else {
-
-					$('#userMail').val(res.userMail);
-
-					var reSendSec = res.reSendSec;
-
-					if (reSendSec == 0) {
-
-						$('#reSendBtn').prop('disabled', false);
-						$('#reSendBtn').text('重寄認證碼');
-
-					} else {
-
-						$('#reSendBtn').prop('disabled', true);
-						$('#reSendBtn').text('重寄認證碼(' + reSendSec + ')');
-
-						var reSendMailInteval = setInterval(function() {
-
-							reSendSec -= 1;
-							$('#reSendBtn').text('重寄認證碼(' + reSendSec + ')');
-
-							if (reSendSec <= 0) {
-
-								$('#reSendBtn').prop('disabled', false);
-								$('#reSendBtn').text('重寄認證碼');
-
-								clearInterval(reSendMailInteval);
-							}
-						}, 1000);
-					}
-				}
+	postSubmit('/verify/mail/buacc', data, function(res) {
+		if(res.status) {
+			if(res.used) {
+				showMsg("您的信箱已經綁定完成！請使用信箱進行登入。");
+				location.href = '/';
 			} else {
-				
-				$('#userMail').val('系統查無資料，請重新綁定');
-				$('#bindBtn').prop('disabled', true);
+				$('#userMail').val(res.userMail);
+				var reSendSec = res.reSendSec;
+				if (reSendSec == 0) {
+					$('#reSendBtn').prop('disabled', false);
+					$('#reSendBtn').text('重寄認證碼');
+				} else {
+					$('#reSendBtn').prop('disabled', true);
+					$('#reSendBtn').text('重寄認證碼(' + reSendSec + ')');
+					var reSendMailInteval = setInterval(function() {
+						reSendSec -= 1;
+						$('#reSendBtn').text('重寄認證碼(' + reSendSec + ')');
+						if (reSendSec <= 0) {
+							$('#reSendBtn').prop('disabled', false);
+							$('#reSendBtn').text('重寄認證碼');
+							clearInterval(reSendMailInteval);
+						}
+					}, 1000);
+				}
 			}
-		},
-		error: function(err) {
-			sysMsg('無法連接伺服器');
+		} else {
+			$('#userMail').val('系統查無資料，請重新綁定');
+			$('#bindBtn').prop('disabled', true);
 		}
 	});
 }
@@ -89,22 +63,12 @@ function bindAct() {
 	data.buAcc = $('#buAcc').val();
 	data.verifyCode = $('#verifyCode').val();
 	
-	$.ajax({
-		url: '/verify/mail/bindAct',
-		method: 'POST',
-		dataType: 'json',
-		contentType: 'application/json',
-		data: JSON.stringify(data),
-		success: function(res) {
-			if(res.status) {
-				showMsg(res.msg);
-				location.href = '/';
-			} else {
-				errMsg(res.msg);
-			}
-		},
-		error: function(err) {
-			sysMsg('無法連接伺服器');
+	postSubmit('/verify/mail/bindAct', data, function(res) {
+		if(res.status) {
+			showMsg(res.msg);
+			location.href = '/';
+		} else {
+			errMsg(res.msg);
 		}
 	});
 }
@@ -117,25 +81,12 @@ function reSendAct() {
 	var data = {};
 	data.buAcc = $('#buAcc').val();
 	
-	$.ajax({
-		url: '/verify/mail/resend',
-		method: 'POST',
-		dataType: 'json',
-		contentType: 'application/json',
-		data: JSON.stringify(data),
-		success: function(res) {
-			
-			if(res.status) {
-				
-				showMsg('已經重新寄發認證碼到您的信箱！請到信箱索取認證碼進行綁定！');
-				
-				initBuAcc();
-			} else {
-				errMsg(res.msg);
-			}
-		},
-		error: function(err) {
-			sysMsg('無法連接伺服器');
+	postSubmit('/verify/mail/resend', data, function(res) {
+		if(res.status) {
+			showMsg('已經重新寄發認證碼到您的信箱！請到信箱索取認證碼進行綁定！');
+			initBuAcc();
+		} else {
+			errMsg(res.msg);
 		}
 	});
 }
