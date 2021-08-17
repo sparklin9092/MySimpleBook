bd69a28a1f1ca78204b556bbfd4b07119da377ee
Spark Lin <sparklin9092@gmail.com>
2021-08-17 13:41:54 +0800

    在共用js -> checkCommon.js 增加 isMobile() 方法，檢查是否為行動裝置

--- src/main/webapp/js/common/checkCommon.js
+++ src/main/webapp/js/common/checkCommon.js
@@ -1,5 +1,5 @@
 /**
- * 檢查訪客目前以建立的資料數量
+ * 共用方法：檢查訪客目前以建立的資料數量
  * 如果超過 5、10、25、50 筆資料以上
  * 就提醒使用者前往綁定帳號
  */
@@ -29,4 +29,33 @@ function checkGuestDataCount() {
  */
 function isFunction(funcToCheck) {
 	return funcToCheck && {}.toString.call(funcToCheck) === '[object Function]';
+}
+
+/**
+ * 共用方法：檢查是否為行動裝置
+ */
+function isMobile() {
+    var isMobile = false;
+	var mobiles = new Array
+    (
+        "midp", "j2me", "avant", "docomo", "novarra", "palmos", "palmsource",
+        "240x320", "opwv", "chtml", "pda", "windows ce", "mmp/",
+        "blackberry", "mib/", "symbian", "wireless", "nokia", "hand", "mobi",
+        "phone", "cdm", "up.b", "audio", "sie-", "sec-", "samsung", "htc",
+        "mot-", "mitsu", "sagem", "sony", "alcatel", "lg", "eric", "vx",
+        "NEC", "philips", "mmm", "xx", "panasonic", "sharp", "wap", "sch",
+        "rover", "pocket", "benq", "java", "pt", "pg", "vox", "amoi",
+        "bird", "compal", "kg", "voda", "sany", "kdd", "dbt", "sendo",
+        "sgh", "gradi", "jb", "dddi", "moto", "iphone", "android",
+        "iPod", "incognito", "webmate", "dream", "cupcake", "webos",
+        "s8000", "bada", "googlebot-mobile"
+    )
+    var ua = navigator.userAgent.toLowerCase();
+    for (var i = 0; i < mobiles.length; i++) {
+        if (ua.indexOf(mobiles[i]) > 0) {
+            isMobile = true;
+            break;
+        }
+    }
+	return isMobile
 }
