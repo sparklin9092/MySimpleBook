94917e30a1a7af0627a7a1f57accc71b2d773b91
Spark Lin <sparklin9092@gmail.com>
2021-11-21 15:24:54 +0800

    Get共用類增加共用方法，去除小數為0的金額

--- src/main/java/net/spark9092/MySimpleBook/common/GetCommon.java
+++ src/main/java/net/spark9092/MySimpleBook/common/GetCommon.java
@@ -10,31 +10,34 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+/**
+ * 各種Get(取得)的共用類
+ * @author spark.9092
+ */
 @Component
 public class GetCommon {
 
 	private static final Logger logger = LoggerFactory.getLogger(GetCommon.class);
 
+	/**
+	 * <b>查請求主機IP地址，如果通過代理進來，則透過防火牆查真實IP地址</b>
+	 * @param request
+	 * @return 真實IP地址
+	 * @throws IOException
+	 */
 	public String getIpAddress(HttpServletRequest request) throws IOException {
-		// 获取请求主机IP地址,如果通过代理进来，则透过防火墙获取真实IP地址
 		String ip = request.getHeader("X-Forwarded-For");
-
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip))
 				ip = request.getHeader("Proxy-Client-IP");
-			}
-			if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip))
 				ip = request.getHeader("WL-Proxy-Client-IP");
-			}
-			if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip))
 				ip = request.getHeader("HTTP_CLIENT_IP");
-			}
-			if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip))
 				ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-			}
-			if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip))
 				ip = request.getRemoteAddr();
-			}
 		} else if (ip.length() > 15) {
 			String[] ips = ip.split(",");
 			for (String ip2 : ips) {
@@ -49,23 +52,26 @@ public class GetCommon {
 	}
 	
 	/**
-	 * 格式化日期，輸出為「yyyy年MM月dd日」
+	 * <b>格式化日期，輸出為「yyyy年MM月dd日」</b>
 	 * @param dataTime (java.time.LocalDateTime)
-	 * @return
+	 * @return 格式化後的日期
 	 */
 	public String getFormatDate(LocalDateTime dataTime) {
-		
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy年MM月dd日");
-		
 		try {
-			
 			return dataTime.format(formatter);
-			
 		} catch (Exception e) {
-			
 			logger.error("格式化日期發生錯誤！！！");
-			
 			return "";
 		}
+	}
+	
+	/**
+	 * <b>傳入金額字串，去除為0的小數; 如果有小數，則保留顯示</b>
+	 * @param amnt
+	 * @return 去除小數為0的金額字串
+	 */
+	public String getNoZeroAmnt(String amnt) {
+		return (amnt.indexOf(".00") == -1) ? amnt : amnt.replace(".00", "");
 	}
 }
