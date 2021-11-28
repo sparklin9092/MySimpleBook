e98e3421d221b821df8172b9b53454689b3de735
Spark Lin <sparklin9092@gmail.com>
2021-11-28 21:32:01 +0800

    對全部的Test Annotation 都進行註解，避免每次maven install都出Error

--- src/test/java/net/spark9092/MySimpleBook/MySimpleBookApplicationTests.java
+++ src/test/java/net/spark9092/MySimpleBook/MySimpleBookApplicationTests.java
@@ -1,16 +1,14 @@
 package net.spark9092.MySimpleBook;
 
-import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MySimpleBookApplicationTests {
 
 	private static final Logger logger = LoggerFactory.getLogger(MySimpleBookApplicationTests.class);
 
-	@Test
+//	@Test
 	void contextLoads() throws Exception {
 		logger.debug("第一個測試");
 	}
--- src/test/java/net/spark9092/MySimpleBook/RichCodeTests.java
+++ src/test/java/net/spark9092/MySimpleBook/RichCodeTests.java
@@ -1,26 +1,21 @@
 package net.spark9092.MySimpleBook;
 
-import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-import net.spark9092.MySimpleBook.service.RichCodeService;
-
-@SpringBootTest
+//@SpringBootTest
 public class RichCodeTests {
 
 	private static final Logger logger = LoggerFactory.getLogger(RichCodeTests.class);
 
-	@Autowired
-	private RichCodeService richCodeService;
+//	@Autowired
+//	private RichCodeService richCodeService;
 
-	@Test
+//	@Test
 	public void getRichCodeList() {
 
 		logger.debug("測試讀取財富密碼的列表");
 
-		richCodeService.getRichCodeList();
+//		richCodeService.getRichCodeList();
 	}
 }
