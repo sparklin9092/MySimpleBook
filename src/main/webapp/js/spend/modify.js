a61caa8a1de673b8286dda5c9725d56c1b28b6a2
Spark Lin <Sparklin9092@gmail.com>
2022-01-25 21:56:30 +0800

    修補修改某一筆支出紀錄時，項目和帳戶下拉選單值錯誤的問題

--- src/main/webapp/js/spend/modify.js
+++ src/main/webapp/js/spend/modify.js
@@ -1,9 +1,6 @@
 $(function() {
 	
 	initSpendDate();
-	initSpendItemSelect();
-	initAccountItemSelect();
-	
 	initData();
 
 	$('#cancelBtn').on('click', cancelAct);
@@ -11,7 +8,6 @@ $(function() {
 	$('#confirmBtn').on('click', confirmAct);
 
 	$('#amount').focus();
-
 	$('#amount').on('keypress', function(e) {
 		var code = (e.keyCode ? e.keyCode : e.which);
 		if (code == 13) $('#confirmBtn').trigger('click');
@@ -19,25 +15,22 @@ $(function() {
 });
 
 function initTodayDate() {
-	
 	$('#spendDatePicker').val(moment().format('YYYY年MM月DD日'));
 	$('#spendDate').val(moment().format('YYYY-MM-DD'));
 }
 
 function initSpendDate() {
-	
 	$('#spendDatePicker').datepicker({
 		dateFormat: 'yy年mm月dd日',
 		showWeek: true,
 		altField: '#spendDate',
 		altFormat: 'yy-mm-dd'
 	});
-	
 	initTodayDate();
 }
 
-function initSpendItemSelect() {
-	
+function initData() {
+	var spendId = $('#spendId').val();
 	postSubmit('/spend/itemList', {}, function(res) {
 		if(res.status) {
 			var itemList = res.itemList;
@@ -46,47 +39,39 @@ function initSpendItemSelect() {
 				selectOptions += '<option value="' + value.itemId + '">' + value.itemName + '</option>';
 			});
 			$('#spendItemSelect').empty().html(selectOptions);
-		} else {
-			errMsg(res.msg);
-		}
-	});
-}
 
-function initAccountItemSelect() {
-	
-	postSubmit('/spend/accountList', {}, function(res) {
-		if(res.status) {
-			var itemList = res.accountList;
-			var selOpts = "";
-			$.each(itemList, function(index, value){
-				selOpts += '<option value="' + value.accountId + '">' + value.accountName + '</option>';
+			postSubmit('/spend/accountList', {}, function(res) {
+				if(res.status) {
+					var itemList = res.accountList;
+					var selOpts = "";
+					$.each(itemList, function(index, value){
+						selOpts += '<option value="' + value.accountId + '">' + value.accountName + '</option>';
+					});
+					$('#accountItemSelect').empty().html(selOpts);
+
+					postSubmit('/spend/one/' + spendId, {}, function(res) {
+						if(res.status) {
+							var spendDate = res.oneDto.spendDate;
+							var amount = res.oneDto.amount;
+							var spendItemId = res.oneDto.spendItemId;
+							var accountId = res.oneDto.accountId;
+							var remark = res.oneDto.remark;
+							
+							$('#spendDatePicker').val(moment(spendDate).format('YYYY年MM月DD日'));
+							$('#spendDate').val(spendDate);
+							$('#amount').val(amount);
+							$('#remark').val(remark);
+							
+							$('#spendItemSelect').val(spendItemId).change();
+							$('#accountItemSelect').val(accountId).change();
+						} else {
+							errMsg(res.msg);
+						}
+					});
+				} else {
+					errMsg(res.msg);
+				}
 			});
-			$('#accountItemSelect').empty().html(selOpts);
-		} else {
-			errMsg(res.msg);
-		}
-	});
-}
-
-function initData() {
-	
-	var spendId = $('#spendId').val();
-	
-	postSubmit('/spend/one/' + spendId, {}, function(res) {
-		if(res.status) {
-			var spendDate = res.oneDto.spendDate;
-			var amount = res.oneDto.amount;
-			var spendItemId = res.oneDto.spendItemId;
-			var accountId = res.oneDto.accountId;
-			var remark = res.oneDto.remark;
-			
-			$('#spendDatePicker').val(moment(spendDate).format('YYYY年MM月DD日'));
-			$('#spendDate').val(spendDate);
-			$('#amount').val(amount);
-			$('#remark').val(remark);
-			
-			$('#spendItemSelect').val(spendItemId).change();
-			$('#accountItemSelect').val(accountId).change();
 		} else {
 			errMsg(res.msg);
 		}
@@ -97,8 +82,7 @@ function cancelAct() {
 	history.back();
 }
 	
-function deleteAct() {
-	
+function deleteAct() {	
 	var deleteConfirm = confirm('確定要刪除嗎？');
 	if(deleteConfirm) {
 		var data = {};
@@ -115,7 +99,6 @@ function deleteAct() {
 }
 
 function confirmAct() {
-	
 	var spendId = $('#spendId').val();
 	var spendDate = $('#spendDate').val();
 	var spendItemId = $('#spendItemSelect').val();
